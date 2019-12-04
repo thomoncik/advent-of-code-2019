329bdad9f6d4e8577a5a8121813bd8cb2d9dd2a8
Tomasz Homoncik <tjhomoncik@gmail.com>
2019-12-04 07:48:41 +0100

    Day 4 tweaks

--- day04.hs
+++ day04.hs
@@ -7,15 +7,11 @@ input = [271973..785961]
 digits :: Int -> [Int]
 digits = map (read . (:[])) . show
 
-isDouble :: [Int] -> Bool
-isDouble [] = False
-isDouble (a:[]) = False
-isDouble (a:b:xs) = if (a == b) then True else isDouble (b:xs)
-
 noDec :: [Int] -> Bool
-noDec (a:[]) = True
-noDec [] = True
-noDec (x1:x2:xs) = if (x1 > x2) then False else noDec (x2:xs)
+noDec l = sort l == l
+
+isDouble :: [Int] -> Bool
+isDouble l = any ((>=2) . length) (group l)
 
 isOk :: Int -> Bool
 isOk x = isDouble (digits x) && noDec (digits x)
@@ -24,7 +20,7 @@ part1 :: Int
 part1 = length (filter isOk input)
 
 isDouble2 :: [Int] -> Bool
-isDouble2 l = any (==2) (map length (group l))
+isDouble2 = any ((2==) . length) . group
 
 isOk2 :: Int -> Bool
 isOk2 x = isDouble2 (digits x) && noDec (digits x)
