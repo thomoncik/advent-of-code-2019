1bb7df4d69f2b10358273fcc24a0d610f8fa039e
Tomasz Homoncik <tjhomoncik@gmail.com>
2019-12-07 20:30:04 +0100

    cleanup, day 2

--- day02.hs
+++ day02.hs
@@ -1,40 +1,40 @@
 module Main (main) where
 import qualified Data.Map as Map
 
-input :: [Int]
+type Program = [Int]
+type Memory = Map.Map Int Int
+
+input :: Program
 input = [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,13,19,23,2,23,9,27,1,6,27,31,2,10,31,35,1,6,35,39,2,9,39,43,1,5,43,47,2,47,13,51,2,51,10,55,1,55,5,59,1,59,9,63,1,63,9,67,2,6,67,71,1,5,71,75,1,75,6,79,1,6,79,83,1,83,9,87,2,87,10,91,2,91,10,95,1,95,5,99,1,99,13,103,2,103,9,107,1,6,107,111,1,111,5,115,1,115,2,119,1,5,119,0,99,2,0,14,0]
 
-replace [] _ _ = []
-replace (_:xs) 0 a = a:xs
-replace (x:xs) n a =
-  if n < 0
-    then (x:xs)
-    else x: (replace xs (n-1) a)
+loadProgramToMemory :: Program -> Memory
+loadProgramToMemory input = Map.fromList (zip [0 ..] input)
 
+f :: Int -> Memory -> Memory
+f cursor l
+  | action == 99 = l
+  | action == 1  = f (cursor + 4) (Map.insert c (a + b) l)
+  | action == 2  = f (cursor + 4) (Map.insert c (a * b) l)
+  | otherwise    = f (cursor + 1) l
+  where
+    action = l Map.! cursor
+    a      = l Map.! (l Map.! (cursor + 1))
+    b      = l Map.! (l Map.! (cursor + 2))
+    c      = l Map.! (cursor + 3)
 
-f :: Int -> [Int] -> [Int]
-f i l
-  | x == 99 = l
-  | x == 1 = f (i+4) (replace l c (a+b))
-  | x == 2 = f (i+4) (replace l c (a*b))
-  | otherwise = f (i+1) l
-  where 
-    x = l !! i
-    a = l !! (l !! (i+1))
-    b = l !! (l !! (i+2))
-    c = l !! (i+3)
+part1 :: Program -> Int
+part1 input = head $ map snd (Map.toList (f 0 (loadProgramToMemory input)))
 
-part1 :: [Int] -> [Int]
-part1 input = f 0 input
+setState :: Memory -> Int -> Int -> Memory
+setState m a b = (Map.insert 2 b (Map.insert 1 a m))
 
-setState :: [Int] -> Int -> Int -> [Int]
-setState (x:_:_:xs) a b = x:a:b:xs
-
-f2 :: [Int] -> (Int,Int) -> Bool
-f2 l (n,v) = head (f 0 (setState l n v)) == 19690720
+f2 :: Memory -> (Int, Int) -> Bool
+f2 l (n, v) = (f 0 (setState l n v)) Map.! 0 == 19690720
 
 part2 :: [Int] -> Int
-part2 l = head [100*n + v | n <- [0..99], v <- [0..99], f2 l (n,v)]
+part2 l = head [100 * n + v | n <- [0 .. 99], v <- [0 .. 99], f2 (loadProgramToMemory l) (n, v)]
 
 main :: IO ()
-main = print $ part2 input
+main = do 
+  print $ part1 input
+  print $ part2 input
