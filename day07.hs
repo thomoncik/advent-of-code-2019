c1230fd628570c0f5662936e0e74976a7ec82cdf
Tomasz Homoncik <tjhomoncik@gmail.com>
2019-12-07 19:52:34 +0100

    Clean up day 7

--- day07.hs
+++ day07.hs
@@ -1,134 +1,156 @@
-module Main (main) where
-import Data.Map as Map
+module Main
+  ( main
+  ) where
+
 import Data.List as List
+import Data.Map as Map
 
-data Mode = Position | Immediate deriving (Show, Eq)
-data Action = JumpTrue Mode Mode
-            | JumpFalse Mode Mode
-            | LessThan Mode Mode
-            | Equals Mode Mode
-            | Halt 
-            | Multiply Mode Mode 
-            | Add Mode Mode 
-            | Input 
-            | Output deriving (Show, Eq)
-type MapIntInt = Map.Map Int Int
+data Mode
+  = Position
+  | Immediate
+  deriving (Show, Eq)
 
+data Action
+  = JumpTrue Mode Mode
+  | JumpFalse Mode Mode
+  | LessThan Mode Mode
+  | Equals Mode Mode
+  | Halt
+  | Multiply Mode Mode
+  | Add Mode Mode
+  | Input
+  | Output
+  deriving (Show, Eq)
 
-input :: [Int]
+type Memory = Map.Map Int Int
+
+type Program = [Int]
+
+type State = (Maybe Int, [Int], [Int], Memory)
+
+input :: Program
 input = [3,8,1001,8,10,8,105,1,0,0,21,38,55,68,93,118,199,280,361,442,99999,3,9,1002,9,2,9,101,5,9,9,102,4,9,9,4,9,99,3,9,101,3,9,9,1002,9,3,9,1001,9,4,9,4,9,99,3,9,101,4,9,9,102,3,9,9,4,9,99,3,9,102,2,9,9,101,4,9,9,102,2,9,9,1001,9,4,9,102,4,9,9,4,9,99,3,9,1002,9,2,9,1001,9,2,9,1002,9,5,9,1001,9,2,9,1002,9,4,9,4,9,99,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99]
 
-listToMap :: [Int] -> MapIntInt
-listToMap input = Map.fromList (zip [0..] input)
+loadProgramToMemory :: Program -> Memory
+loadProgramToMemory input = Map.fromList (zip [0 ..] input)
 
-digits :: Int -> [Int]
-digits = List.map (read . (:[])) . show
+digitFromRight :: Int -> Int -> Int
+digitFromRight n x = (x `div` (10 ^ n)) `mod` 10
 
-parseParam :: Int -> Action
-parseParam x = parseParamReverseList (reverse (digits x))
+parseToAction :: Int -> Action
+parseToAction x = getAction $ getMode
+  where
+    getMode
+      | b == 0 && c == 0 = (Immediate, Immediate)
+      | b == 0 && c == 1 = (Immediate, Position)
+      | b == 1 && c == 0 = (Position, Immediate)
+      | b == 1 && c == 1 = (Position, Position)
+    getAction (mode1, mode2)
+      | d == 9 && e == 9 = Halt
+      | d == 0 && e == 8 = Equals mode1 mode2
+      | d == 0 && e == 7 = LessThan mode1 mode2
+      | d == 0 && e == 6 = JumpFalse mode1 mode2
+      | d == 0 && e == 5 = JumpTrue mode1 mode2
+      | d == 0 && e == 4 = Output
+      | d == 0 && e == 3 = Input
+      | d == 0 && e == 2 = Multiply mode1 mode2
+      | d == 0 && e == 1 = Add mode1 mode2
+    a = digitFromRight 4 x
+    b = digitFromRight 3 x
+    c = digitFromRight 2 x
+    d = digitFromRight 1 x
+    e = digitFromRight 0 x
 
-parseParamReverseList :: [Int] -> Action
-parseParamReverseList (1:0:1:1:_) = Add Immediate Immediate
-parseParamReverseList (1:0:0:1:_) = Add Position Immediate
-parseParamReverseList (1:0:1:_) = Add Immediate Position
-parseParamReverseList (1:_) = Add Position Position
+programStep :: State -> State
+programStep (Just cursor, pins, diag, l) = afterAction action cursor l diag pins
+  where
+    afterAction Halt cursor memory outputs inputs =
+      (Nothing, inputs, outputs, memory)
+    afterAction Output cursor memory outputs inputs =
+      (Just (cursor + 2), inputs, (arg 1 Position) : outputs, memory)
+    afterAction Input cursor memory diag pins =
+      ( Just (cursor + 2)
+      , (tail pins)
+      , diag
+      , (Map.insert (arg 1 Immediate) (head pins) memory))
+    afterAction (Add m1 m2) cursor l diag pins =
+      ( Just (cursor + 4)
+      , pins
+      , diag
+      , (Map.insert (arg 3 Immediate) ((arg 1 m1) + (arg 2 m2)) l))
+    afterAction (Multiply mode1 mode2) cursor l diag pins =
+      ( Just (cursor + 4)
+      , pins
+      , diag
+      , (Map.insert (arg 3 Immediate) ((arg 1 mode1) * (arg 2 mode2)) l))
+    afterAction (JumpTrue mode1 mode2) cursor l diag pins
+      | arg 1 mode1 /= 0 = (Just (arg 2 mode2), pins, diag, l)
+      | otherwise = (Just (cursor + 3), pins, diag, l)
+    afterAction (JumpFalse mode1 mode2) cursor l diag pins
+      | (arg 1 mode1) == 0 = (Just (arg 2 mode2), pins, diag, l)
+      | otherwise = (Just (cursor + 3), pins, diag, l)
+    afterAction (LessThan mode1 mode2) cursor l diag pins
+      | (arg 1 mode1) < (arg 2 mode2) =
+        (Just (cursor + 4), pins, diag, (Map.insert (arg 3 Immediate) 1 l))
+      | otherwise =
+        (Just (cursor + 4), pins, diag, (Map.insert (arg 3 Immediate) 0 l))
+    afterAction (Equals mode1 mode2) cursor l diag pins
+      | (arg 1 mode1) == (arg 2 mode2) =
+        (Just (cursor + 4), pins, diag, (Map.insert (arg 3 Immediate) 1 l))
+      | otherwise =
+        (Just (cursor + 4), pins, diag, (Map.insert (arg 3 Immediate) 0 l))
+    action = parseToAction (l ! cursor)
+    arg number mode
+      | mode == Position = l ! (l ! (cursor + number))
+      | otherwise = l ! (cursor + number)
 
-parseParamReverseList (2:0:1:1:_) = Multiply Immediate Immediate
-parseParamReverseList (2:0:0:1:_) = Multiply Position Immediate
-parseParamReverseList (2:0:1:_) = Multiply Immediate Position
-parseParamReverseList (2:_) = Multiply Position Position
+runWhileNotHalted :: State -> State
+runWhileNotHalted (Nothing, inputs, outputs, memory) =
+  (Nothing, inputs, outputs, memory)
+runWhileNotHalted (Just cursor, inputs, outputs, memory) =
+  runWhileNotHalted $ programStep (Just cursor, inputs, outputs, memory)
 
-parseParamReverseList (3:_) = Input
-parseParamReverseList (4:_) = Output
+programOutput :: Program -> [Int] -> [Int]
+programOutput program inputs =
+  reverse $
+  (\(a, b, c, d) -> c) $
+  runWhileNotHalted $
+  (programStep (Just 0, inputs, [], (loadProgramToMemory program)))
 
-parseParamReverseList (5:0:1:1:_) = JumpTrue Immediate Immediate
-parseParamReverseList (5:0:0:1:_) = JumpTrue Position Immediate
-parseParamReverseList (5:0:1:_) = JumpTrue Immediate Position
-parseParamReverseList (5:_) = JumpTrue Position Position
+-----------------------------------------
 
-parseParamReverseList (6:0:1:1:_) = JumpFalse Immediate Immediate
-parseParamReverseList (6:0:0:1:_) = JumpFalse Position Immediate
-parseParamReverseList (6:0:1:_) = JumpFalse Immediate Position
-parseParamReverseList (6:_) = JumpFalse Position Position
+signalAfterChain :: [Int] -> Program -> Int
+signalAfterChain settings program = setCompontents settings
+  where
+    setCompontents [a, b, c, d, e] =
+      let aResult = programOutput program [a, 0]
+          bResult = programOutput program (b : aResult)
+          cResult = programOutput program (c : bResult)
+          dResult = programOutput program (d : cResult)
+          eResult = programOutput program (e : dResult)
+       in last eResult
 
-parseParamReverseList (7:0:1:1:_) = LessThan Immediate Immediate
-parseParamReverseList (7:0:0:1:_) = LessThan Position Immediate
-parseParamReverseList (7:0:1:_) = LessThan Immediate Position
-parseParamReverseList (7:_) = LessThan Position Position
-
-parseParamReverseList (8:0:1:1:_) = Equals Immediate Immediate
-parseParamReverseList (8:0:0:1:_) = Equals Position Immediate
-parseParamReverseList (8:0:1:_) = Equals Immediate Position
-parseParamReverseList (8:_) = Equals Position Position
-
-parseParamReverseList (9:9:_) = Halt
-parseParamReverseList x = error (show x)
-
-f :: Int -> MapIntInt -> [Int] -> [Int] -> (MapIntInt, [Int])
-f i l diag pins
-  | x == Halt   = (l, diag)
-  | x == Output = f (i+2) l (ap:diag) pins
-  | x == Input  = f (i+2) (Map.update (\y-> Just (head pins)) a l) diag (tail pins)
-  | x == (Add Position Position)        = f (i+4) (Map.update (\y-> Just (ap+bp)) c l) diag pins
-  | x == (Add Immediate Position)       = f (i+4) (Map.update (\y-> Just (a+bp)) c l) diag pins
-  | x == (Add Position Immediate)       = f (i+4) (Map.update (\y-> Just (ap+b)) c l) diag pins
-  | x == (Add Immediate Immediate)      = f (i+4) (Map.update (\y-> Just (a+b)) c l) diag pins
-  | x == (Multiply Position Position)   = f (i+4) (Map.update (\y-> Just (ap*bp)) c l) diag pins
-  | x == (Multiply Immediate Position)  = f (i+4) (Map.update (\y-> Just (a*bp)) c l) diag pins
-  | x == (Multiply Position Immediate)  = f (i+4) (Map.update (\y-> Just (ap*b)) c l) diag pins
-  | x == (Multiply Immediate Immediate) = f (i+4) (Map.update (\y-> Just (a*b)) c l) diag pins
-  | x == (JumpTrue Position Position)   = if ap /= 0 then f bp l diag pins else f (i+3) l diag pins
-  | x == (JumpTrue Immediate Position)  = if a /= 0 then f bp l diag pins else f (i+3) l diag pins
-  | x == (JumpTrue Position Immediate)  = if ap /= 0 then f b l diag pins else f (i+3) l diag pins
-  | x == (JumpTrue Immediate Immediate) = if a /= 0 then f b l diag pins else f (i+3) l diag pins
-  | x == (JumpFalse Position Position)   = if ap == 0 then f bp l diag pins else f (i+3) l diag pins
-  | x == (JumpFalse Immediate Position)  = if a == 0 then f bp l diag pins else f (i+3) l diag pins
-  | x == (JumpFalse Position Immediate)  = if ap == 0 then f b l diag pins else f (i+3) l diag pins
-  | x == (JumpFalse Immediate Immediate) = if a == 0 then f b l diag pins else f (i+3) l diag pins
-  | x == (LessThan Position Position)   = if ap < bp then f (i+4) (Map.update (\y-> Just 1) c l) diag pins else f (i+4) (Map.update (\y-> Just 0) c l) diag pins
-  | x == (LessThan Immediate Position)  = if a < bp then f (i+4) (Map.update (\y-> Just 1) c l) diag pins else f (i+4) (Map.update (\y-> Just 0) c l) diag pins
-  | x == (LessThan Position Immediate)  = if ap < b then f (i+4) (Map.update (\y-> Just 1) c l) diag pins else f (i+4) (Map.update (\y-> Just 0) c l) diag pins
-  | x == (LessThan Immediate Immediate) = if a < b then f (i+4) (Map.update (\y-> Just 1) c l) diag pins else f (i+4) (Map.update (\y-> Just 0) c l) diag pins
-  | x == (Equals Position Position)   = if ap == bp then f (i+4) (Map.update (\y-> Just 1) c l) diag pins else f (i+4) (Map.update (\y-> Just 0) c l) diag pins
-  | x == (Equals Immediate Position)  = if a == bp then f (i+4) (Map.update (\y-> Just 1) c l) diag pins else f (i+4) (Map.update (\y-> Just 0) c l) diag pins
-  | x == (Equals Position Immediate)  = if ap == b then f (i+4) (Map.update (\y-> Just 1) c l) diag pins else f (i+4) (Map.update (\y-> Just 0) c l) diag pins
-  | x == (Equals Immediate Immediate) = if a == b then f (i+4) (Map.update (\y-> Just 1) c l) diag pins else f (i+4) (Map.update (\y-> Just 0) c l) diag pins
-  where 
-    x = parseParam $ l Map.! i
-    a = l Map.! (i+1)
-    ap = l Map.! a
-    b = l Map.! (i+2)
-    bp = l Map.! b
-    c = l Map.! (i+3)
-
-
-part1 = maximum (List.map (\p -> s p input) (permutations [0..4]))
+part1 = maximum (List.map (\p -> signalAfterChain p input) (permutations [0 .. 4]))
 
 --------------------------
 
-fAmp :: [Int] -> [Int] -> [Int]
-fAmp pins prog = reverse $ snd (f 0 (listToMap prog) [] pins)
-
-part2 :: Int
-part2 = maximum (List.map (\p -> s2 p input) (permutations [5..9]))
-
-s :: [Int] -> [Int] -> Int
-s sett prog = calculate sett
-  where calculate [a, b, c, d, e] = let oa = fAmp [a,0] prog
-                                        ob = fAmp (b:oa) prog
-                                        oc = fAmp (c:ob) prog
-                                        od = fAmp (d:oc) prog
-                                        oe = fAmp (e:od) prog
-                                    in last oe
 s2 :: [Int] -> [Int] -> Int
 s2 sett prog = calculate sett
-  where calculate [a, b, c, d, e] = let oa = fAmp (a:0:oe) prog
-                                        ob = fAmp (b:oa) prog
-                                        oc = fAmp (c:ob) prog
-                                        od = fAmp (d:oc) prog
-                                        oe = fAmp (e:od) prog
-                                    in last oe
-main :: IO ()
-main = print ""
--- main = print $ part2 [-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
+  where
+    calculate [a, b, c, d, e] =
+      let oa = programOutput prog (a : 0 : oe)
+          ob = programOutput prog (b : oa)
+          oc = programOutput prog (c : ob)
+          od = programOutput prog (d : oc)
+          oe = programOutput prog (e : od)
+       in last oe
 
+part2 :: Int
+part2 = maximum (List.map (\p -> s2 p input) (permutations [5 .. 9]))
+
+---------------------------
+
+main :: IO ()
+main = do
+  print part1
+  print part2
